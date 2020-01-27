f91bea00fa4acf707039cf62d74ba43d42e34652
Michal Zet <michal.zadarnowski@gmail.com>
2020-01-27 07:31:13 +0000

    007 - cypress test card number

--- cypress/integration/app/assertions.spec.js
+++ cypress/integration/app/assertions.spec.js
@@ -1,18 +1,17 @@
 import '@testing-library/cypress/add-commands';
 /// <reference types="Cypress" />
 
-
 context('Assertions', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:3000')
-  })
+	beforeEach(() => {
+		cy.visit('http://localhost:3000');
+	});
 
-  describe('Test radio selection', () => {
-    it('should show correct number of card in radio selection', () => {
-      cy.findByLabelText('5')
-        .click()
-        .findAllByTestId('players-list-item')
-        .should('have.length', 5)
-    })
-  })
-  
+	describe('Test radio selection', () => {
+		it('should show correct number of card in radio selection', () => {
+			cy.findByLabelText('5')
+				.click()
+				.findAllByTestId('players-list-item')
+				.should('have.length', 5);
+		});
+	});
+});
