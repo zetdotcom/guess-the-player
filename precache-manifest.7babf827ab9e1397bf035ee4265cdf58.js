29ffadcc2bfa3fb80a1717c83f23542e54c533bf
Michal Zet <michal.zadarnowski@gmail.com>
2020-01-27 19:57:29 +0000

    Updates

---
+++ precache-manifest.7babf827ab9e1397bf035ee4265cdf58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ab237ff1f7ae2cefed7aca2d225d664",
+    "url": "/guess-the-player/index.html"
+  },
+  {
+    "revision": "fc0b60b46f9030b66c56",
+    "url": "/guess-the-player/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "59913686463fe0874b92",
+    "url": "/guess-the-player/static/js/2.ce9b0837.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/guess-the-player/static/js/2.ce9b0837.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc0b60b46f9030b66c56",
+    "url": "/guess-the-player/static/js/main.69b9f568.chunk.js"
+  },
+  {
+    "revision": "bb1c0233c157a0d01e60",
+    "url": "/guess-the-player/static/js/runtime-main.542d994e.js"
+  }
+]);
